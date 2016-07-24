cdc3360a7da1a1cafe4f75a29fe93bcf6ef3a53b
Necto <necto.ne@gmail.com>
2016-07-24 17:50:24 +0200

    Call path and call prefixes are reported now only for healthy states (with no crash discovered), and skipping the loop-analysis states.

--- lib/Core/ExecutionState.cpp
+++ lib/Core/ExecutionState.cpp
@@ -76,13 +76,15 @@ ExecutionState::ExecutionState(KFunction *kf) :
     instsSinceCovNew(0),
     coveredNew(false),
     forkDisabled(false),
-    ptreeNode(0) {
+    ptreeNode(0),
+    erroneous(false) {
   pushFrame(0, kf);
 }
 
 ExecutionState::ExecutionState(const std::vector<ref<Expr> > &assumptions)
   : executionStateForLoopInProcess(0), constraints(assumptions),
-    queryCost(0.), ptreeNode(0) {}
+    queryCost(0.), ptreeNode(0),
+    erroneous(false) {}
 
 ExecutionState::~ExecutionState() {
   for (unsigned int i=0; i<symbolics.size(); i++)
@@ -125,7 +127,8 @@ ExecutionState::ExecutionState(const ExecutionState& state):
     symbolics(state.symbolics),
     arrayNames(state.arrayNames),
     openMergeStack(state.openMergeStack),
-    callPath(state.callPath)
+    callPath(state.callPath),
+    erroneous(state.erroneous)
 {
   for (unsigned int i=0; i<symbolics.size(); i++)
     symbolics[i].first->refCount++;
@@ -888,7 +891,7 @@ void LoopInProcess::updateChangedObjects(const ExecutionState& current,
         // it also differs structurally now. It is time to make
         // sure it can be really different.
 
-        solver->setTimeout(10);//TODO: determine a correct argument here.
+        solver->setTimeout(0.01);//TODO: determine a correct argument here.
         bool mayDiffer = true;
         bool solverRes = solver->mayBeFalse(current, EqExpr::create(headVal, beVal),
                                             /*&*/mayDiffer);
