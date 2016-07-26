0d955b255a5e8466395e52a4c29f443510ca629d
Necto <necto.ne@gmail.com>
2016-07-26 15:33:05 +0200

    Fixed loop nesting handling for the case when the inner loop is not analysed.

--- lib/Core/ExecutionState.cpp
+++ lib/Core/ExecutionState.cpp
@@ -636,6 +636,7 @@ void ExecutionState::loopEnter(const llvm::Loop *dstLoop) {
   LOG_LA("store the loop-head entering state,"
          " just in case.");
   executionStateForLoopInProcess = branch();
+  executionStateForLoopInProcess->loopInProcess = 0;
 }
 
 void ExecutionState::loopExit(const llvm::Loop *srcLoop,
@@ -656,8 +657,6 @@ void ExecutionState::updateLoopAnalysisForBlockTransfer
                       (BasicBlock *dst, BasicBlock *src,
                        TimingSolver* solver,
                        bool *terminate) {
-  //TODO: support PHI functions on the loop entrance.
-
   KFunction *kf = stack.back().kf;
   const llvm::Loop *dstLoop = kf->loopInfo.getLoopFor(dst);
   const llvm::Loop *srcLoop = kf->loopInfo.getLoopFor(src);
