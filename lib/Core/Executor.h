b0072edaaa393e2a5ff329c5cae32879413be25f
Necto <necto.ne@gmail.com>
2016-07-26 13:21:59 +0200

    Refactor: move the klee_induce_invariants handling completely to the ExecutionState side

--- lib/Core/Executor.h
+++ lib/Core/Executor.h
@@ -354,9 +354,6 @@ private:
     return state.stack.back().locals[target->dest];
   }
 
-  void induceInvariantsForThisLoop(ExecutionState &state,
-                                   KInstruction *target);
-
   void bindLocal(KInstruction *target, 
                  ExecutionState &state, 
                  ref<Expr> value);
