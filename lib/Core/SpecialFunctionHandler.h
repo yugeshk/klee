007f2f79cac19bec8c9d13242008fd0f74a370f7
Necto <necto.ne@gmail.com>
2016-08-29 12:40:59 +0200

    the new feature initial commit: klee_forbid_access/klee_allow_access for fine controll over memory. This combination allows to disable access to certain location during certain periods, e.g. outside of a module.

--- lib/Core/SpecialFunctionHandler.h
+++ lib/Core/SpecialFunctionHandler.h
@@ -153,6 +153,8 @@ namespace klee {
     HANDLER(handleTraceRetPtrNestedField);
     HANDLER(handleForgetAll);
     HANDLER(handleInduceInvariants);
+    HANDLER(handleForbidAccess);
+    HANDLER(handleAllowAccess);
 #undef HANDLER
   };
 } // End klee namespace
