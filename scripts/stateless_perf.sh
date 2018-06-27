d11fd57df9f56f4c73cf2ddd746ba844990d4935
rishabh246 <rishabh246@gmail.com>
2018-06-27 15:39:10 +0200

    Added stubs for verify-dpdk and verify-hw

--- scripts/stateless_perf.sh
+++ scripts/stateless_perf.sh
@@ -5,13 +5,19 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 py_scripts_dir=$SCRIPT_DIR/trace_scripts
 traces_dr=${1:-klee-last} #traces_dir needs to contain files with the name *.packet_relevant_instructions 
 output=${2:-stateless-perf.txt}
-
+verif_arg=${3:-verify-dpdk}
 
 cd $traces_dr
 
 parallel "$py_scripts_dir/process_trace.sh {} \$(basename {} .instructions).packet_relevant_instructions" ::: *.instructions
 
-python $py_scripts_dir/demarcate_trace.py ./ $py_scripts_dir/fn_lists/stateful_fns.txt $py_scripts_dir/fn_lists/dpdk_fns.txt $py_scripts_dir/fn_lists/time_fns.txt $py_scripts_dir/fn_lists/verif_fns.txt 
+if [ "$verif_arg" == "verify-dpdk" ]; then
+ stub_file=$py_scripts_dir/fn_lists/dpdk_fns.txt
+else
+ stub_file=$py_scripts_dir/fn_lists/hardware_fns.txt
+fi
+
+python $py_scripts_dir/demarcate_trace.py ./ $py_scripts_dir/fn_lists/stateful_fns.txt $stub_file  $py_scripts_dir/fn_lists/time_fns.txt $py_scripts_dir/fn_lists/verif_fns.txt 
 python $py_scripts_dir/print_addresses.py ./
 python $py_scripts_dir/formal_cache.py ./
 python $py_scripts_dir/stateless_stats.py ./ comp_insns num_accesses num_hits num_misses trace_nos
