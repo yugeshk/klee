9d0a26faf76081d243c3220231101c2ee4ad5631
Luis D. Pedrosa <luis.pedrosa@epfl.ch>
2018-07-09 17:25:36 +0000

    Added script to process a sequence of NFs from their call_paths.

---
+++ scripts/sequence-nfs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SENDER_TRACES_DIR=$1
+RECEIVER_TRACES_DIR=$2
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+parallel 'check-call-path-compatibility {1} {2} >/dev/null 2>/dev/null && echo $(basename {1} .call_path),$(basename {2} .call_path) || true' \
+    ::: $SENDER_TRACES_DIR/*.call_path \
+    ::: $RECEIVER_TRACES_DIR/*.call_path \
+    | sort > compatible-paths.csv
+
+join -t , -1 1 -2 1 <(sort $SENDER_TRACES_DIR/stateless-perf.txt) <(sort $SENDER_TRACES_DIR/stateful-perf.txt) \
+    | awk '
+      BEGIN { FS=","; OFS="," }
+      $2 == $4 { print $1,$2,$3,$5 }' \
+    | sort \
+    > $SENDER_TRACES_DIR/all-perf.csv
+
+join -t , -1 1 -2 1 <(sort $RECEIVER_TRACES_DIR/stateless-perf.txt) <(sort $RECEIVER_TRACES_DIR/stateful-perf.txt) \
+    | awk '
+      BEGIN { FS=","; OFS="," }
+      $2 == $4 { print $1,$2,$3,$5 }' \
+    | sort \
+    > $RECEIVER_TRACES_DIR/all-perf.csv
+
+join -t , -1 1 -2 1 compatible-paths.csv $SENDER_TRACES_DIR/all-perf.csv \
+    | sort -t , -k 2 \
+    | join -t , -1 2 -2 1 - $RECEIVER_TRACES_DIR/all-perf.csv \
+    | awk '
+      BEGIN { FS=","; OFS="," }
+      $3 == $6 { print $2,$1,$3,$4,$5,$7,$8,($4+$5+$7+$8) }' \
+    | sort \
+    > sequence-perf.csv
