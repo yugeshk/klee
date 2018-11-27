54277a03620adef914f408e050a6dfa973debf95
rishabh246 <rishabh246@gmail.com>
2018-11-27 13:43:27 +0100

    Simulated first instruction

---
+++ scripts/abstract-interpretation/exec_state.py
@@ -0,0 +1,5 @@
+class ExecutionState:
+
+	def __init__(self, regs, mem):
+    	  self.regs = regs  #Values of all the registers 
+    	  self.mem = mem    # Value of all the memory named by the process
--- scripts/abstract-interpretation/instruction.py
+++ scripts/abstract-interpretation/instruction.py
@@ -1,8 +1,11 @@
 class Ins:
 
-	def _init_(self,category,opcode,IP,disass,regs)
-		self.IP = IP    	    # Instruction pointer
-		self.regs = regs	    # Value of all registers before this instruction was executed 
-		self.category = category    # Instruction category
-		self.opcode = opcode	    # Opcode	 	
-		self.disass = disass 	    # Disassembled instruction 	
+
+	def __init__(self,category,opcode,IP,disass,regs,addresses):
+	  self.IP = IP    	    # Instruction pointer
+	  self.regs = regs	    # Value of all registers before this instruction was executed 
+	  self.category = category    # Instruction category
+	  self.opcode = opcode	    # Opcode	 	
+	  self.disass = disass 	    # Disassembled instruction
+	  self.addresses = addresses  # Addresses read to and written from 
+
