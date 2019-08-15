c10633df854aa0dd0374745b01e0509c6ed31f0e
rishabh246 <rishabh246@gmail.com>
2019-08-15 09:28:46 +0200

    Updated to python 3

--- scripts/stateless_scripts/cache_setup.py
+++ scripts/stateless_scripts/cache_setup.py
@@ -15,9 +15,9 @@ cache_size = 32768
 cache_block_size = 64
 set_associativity = 8
 set_size = cache_block_size*set_associativity
-num_sets = cache_size/set_size
-cache_contents = [[] for _ in xrange(num_sets)]
-cache_ages = [[] for _ in xrange(num_sets)]
+num_sets = cache_size//set_size  # Integer division
+cache_contents = [[] for _ in range(num_sets)]
+cache_ages = [[] for _ in range(num_sets)]
 
 # Assuming replacement policy is LRU
 
@@ -38,7 +38,7 @@ def main():
                 age_cache_contents()
             elif(not m2):
                 addr = int(text, 16)
-                block_no = addr/cache_block_size
+                block_no = addr//cache_block_size  # Integer division
                 set_no = block_no % num_sets
                 if block_no not in cache_contents[set_no]:
                     cache_contents[set_no].append(block_no)
@@ -51,8 +51,8 @@ def main():
 def age_cache_contents():
     global cache_contents
     global cache_ages
-    for x in xrange(len(cache_ages)):
-        for y in xrange(len(cache_ages[x])):
+    for x in range(len(cache_ages)):
+        for y in range(len(cache_ages[x])):
             # cache_ages[x][y]=set_associativity+1 # Clear cache
             cache_ages[x][y] = cache_ages[x][y]  # Don't clear cache.
 
@@ -62,7 +62,10 @@ def update_ages(block_num, set_num):
     global cache_ages
     index = cache_contents[set_num].index(block_num)
     age = cache_ages[set_num][index]
-    for x in xrange(len(cache_ages[set_num])):
+    if (age == None):
+        # This is for a new block, since all existing blocks must be aged
+        age = set_associativity+1
+    for x in range(len(cache_ages[set_num])):
         if(x == index):
             cache_ages[set_num][x] = 0
         elif(not(x == index) and cache_ages[set_num][x] <= age):
@@ -73,8 +76,8 @@ def print_cache_contents(op_file):
     global cache_contents
     global cache_ages
     with open(op_file, "w") as output:
-        for x in xrange(len(cache_ages)):
-            for y in xrange(len(cache_ages[x])):
+        for x in range(len(cache_ages)):
+            for y in range(len(cache_ages[x])):
                 if(cache_ages[x][y] < set_associativity):
                     output.write(str(hex(cache_contents[x][y]))+"\n")
 
--- scripts/stateless_scripts/formal_cache.py
+++ scripts/stateless_scripts/formal_cache.py
@@ -1,5 +1,5 @@
 # Script to analyze a sequence of memory address using a simple 1 level, set associative LRU cache.
-# 
+#
 # $1: Input trace of memory addresses
 # $2: Output file, with each memory access tagged as a hit or a miss
 
@@ -16,9 +16,9 @@ cache_size = 32768
 cache_block_size = 64
 set_associativity = 8
 set_size = cache_block_size*set_associativity
-num_sets = cache_size/set_size
-cache_contents = [[] for _ in xrange(num_sets)]
-cache_ages = [[] for _ in xrange(num_sets)]
+num_sets = cache_size//set_size  # Integer division
+cache_contents = [[] for _ in range(num_sets)]
+cache_ages = [[] for _ in range(num_sets)]
 
 # Assuming replacement policy is LRU
 
@@ -48,7 +48,7 @@ def main():
                     output.write(text+"\n")
                 else:
                     addr = int(text, 16)
-                    block_no = addr/cache_block_size
+                    block_no = addr//cache_block_size  # Integer division
                     set_no = block_no % num_sets
                     if block_no not in cache_contents[set_no] and block_no in common_lines:
                         cache_contents[set_no].append(block_no)
@@ -68,8 +68,8 @@ def main():
 def age_cache_contents():
     global cache_contents
     global cache_ages
-    for x in xrange(len(cache_ages)):
-        for y in xrange(len(cache_ages[x])):
+    for x in range(len(cache_ages)):
+        for y in range(len(cache_ages[x])):
             # cache_ages[x][y]=set_associativity+1 # Clear cache
             cache_ages[x][y] = cache_ages[x][y]  # Don't clear cache.
 
@@ -79,7 +79,10 @@ def update_ages(block_num, set_num):
     global cache_ages
     index = cache_contents[set_num].index(block_num)
     age = cache_ages[set_num][index]
-    for x in xrange(len(cache_ages[set_num])):
+    if (age == None):
+        # This is for a new block, since all existing blocks must be aged
+        age = set_associativity+1
+    for x in range(len(cache_ages[set_num])):
         if(x == index):
             cache_ages[set_num][x] = 0
         elif(not(x == index) and cache_ages[set_num][x] <= age):
