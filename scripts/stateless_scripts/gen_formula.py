4efc738fae4c11315b11fb70d11d87e7f5c98163
rishabh246 <rishabh246@gmail.com>
2019-09-09 13:57:43 +0200

    Print formula in nice order

--- scripts/stateless_scripts/gen_formula.py
+++ scripts/stateless_scripts/gen_formula.py
@@ -17,38 +17,52 @@ stateless_perf = {}
 stateful_perf = {}
 metrics = ["instruction count", "memory instructions", "execution cycles"]
 
-with open(stateless_file, 'r') as sless_file:
-    for line in sless_file:
-        line = line.rstrip()
-        line = line.split(',')
-        assert(len(line) == 3)  # Something wrong with output format
-        if(line[0] in stateless_perf):
-            stateless_perf[line[0]][line[1]] = line[2]
-        else:
-            stateless_perf[line[0]] = {}
-            stateless_perf[line[0]][line[1]] = line[2]
 
-with open(stateful_file, 'r') as sful_file:
-    for line in sful_file:
-        line = line.rstrip()
-        line = line.split(',')
-        assert(len(line) == 3)  # Something wrong with output format
-        if(line[0] in stateful_perf):
-            stateful_perf[line[0]][line[1]] = line[2]
-        else:
-            stateful_perf[line[0]] = {}
-            stateful_perf[line[0]][line[1]] = line[2]
+def formula_priority_fn(x):
+    if("e" in x):  # Put expiry first
+        return 0
+    if("*" in x):  # Put collisions and traversals second
+        return 1
+    return 2  # Put constant last
 
-with open(op_file, "w") as op:
-    for key, val in stateful_perf.items():
-        for metric in metrics:
-            formula = val[metric].split('+')
-            final_formula = ""
-            for term in formula:
-                term = term.strip()
-                if("*" not in term):
-                    term = str(int(term)+int(stateless_perf[key][metric]))
-                if(final_formula != ""):
-                    final_formula = final_formula + " + "
-                final_formula = final_formula + term
-            op.write("%s,%s,%s\n" % (key, metric, final_formula))
+
+def main():
+    with open(stateless_file, 'r') as sless_file:
+        for line in sless_file:
+            line = line.rstrip()
+            line = line.split(',')
+            assert(len(line) == 3)  # Something wrong with output format
+            if(line[0] in stateless_perf):
+                stateless_perf[line[0]][line[1]] = line[2]
+            else:
+                stateless_perf[line[0]] = {}
+                stateless_perf[line[0]][line[1]] = line[2]
+
+    with open(stateful_file, 'r') as sful_file:
+        for line in sful_file:
+            line = line.rstrip()
+            line = line.split(',')
+            assert(len(line) == 3)  # Something wrong with output format
+            if(line[0] in stateful_perf):
+                stateful_perf[line[0]][line[1]] = line[2]
+            else:
+                stateful_perf[line[0]] = {}
+                stateful_perf[line[0]][line[1]] = line[2]
+
+    with open(op_file, "w") as op:
+        for key, val in stateful_perf.items():
+            for metric in metrics:
+                formula = val[metric].split('+')
+                formula.sort(key=formula_priority_fn)
+                final_formula = ""
+                for term in formula:
+                    term = term.strip()
+                    if("*" not in term):
+                        term = str(int(term)+int(stateless_perf[key][metric]))
+                    if(final_formula != ""):
+                        final_formula = final_formula + " + "
+                    final_formula = final_formula + term
+                op.write("%s,%s,%s\n" % (key, metric, final_formula))
+
+
+main()
