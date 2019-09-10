9db53bc2cf081e001a59f3a020be49b01e5d4558
rishabh246 <rishabh246@gmail.com>
2019-09-10 15:16:25 +0200

    Do it for all metrics at once

--- scripts/contract-tree/build_tree.sh
+++ scripts/contract-tree/build_tree.sh
@@ -8,7 +8,13 @@ TRACES_DIR=${1:-klee-last}
 pushd $TRACES_DIR
 
 grep "TRAFFIC_CLASS" *.call_path | awk -F: '{print $1 "," $2}' | awk -F' = ' '{print $1 "," $2}' | sed 's/\.call_path//g' > tc_tags
-python $KLEE_DIR/scripts/contract-tree/build_tree.py tc_tags combined_perf.txt perf-formula.txt "instruction count" tree.txt 0 perf_var formula_var
+METRICS=("instruction count" "memory instructions" "execution cycles")
+for METRIC in "${METRICS[@]}"; 
+do 
+  echo "$METRIC"
+  METRIC_NAME=$(echo "$METRIC" | sed -e 's/ /_/')
+  python $KLEE_DIR/scripts/contract-tree/build_tree.py tc_tags combined_perf.txt perf-formula.txt "$METRIC" tree.txt 0 perf_var_$METRIC_NAME formula_var_$METRIC_NAME 
+done
 
 popd
 
