0ab43b7625b9380bb5aa4d665b19a321b95adcb8
rishabh246 <rishabh246@gmail.com>
2019-11-20 10:51:58 +0100

    Accept resolution as parameter

--- scripts/contract-tree/build_tree.sh
+++ scripts/contract-tree/build_tree.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 
 KLEE_DIR=~/projects/Bolt/klee
 TREE_TYPE=$1
-TRACES_DIR=${2:-klee-last}
+RESOLUTION=$2
+TRACES_DIR=${3:-klee-last}
 
 if [ "$TREE_TYPE" != "call-tree" ] && [ "$TREE_TYPE" != "full-tree" ]; then
   echo "Unsupported tree type: $TREE_TYPE"
@@ -26,7 +27,7 @@ METRICS=("instruction count")
 for METRIC in "${METRICS[@]}"; 
 do 
   METRIC_NAME=$(echo "$METRIC" | sed -e 's/ /_/')
-  python $KLEE_DIR/scripts/contract-tree/build_tree.py tc_tags combined_perf.txt perf-formula.txt "$METRIC" $TREE_FILE $TREE_TYPE 0 perf_var_$METRIC_NAME formula_var_$METRIC_NAME 
+  python $KLEE_DIR/scripts/contract-tree/build_tree.py tc_tags combined_perf.txt perf-formula.txt "$METRIC" $TREE_FILE $TREE_TYPE $RESOLUTION perf_var_$METRIC_NAME formula_var_$METRIC_NAME 
 done
 
 popd
