ffb16ef155b44ca3d3c7ff65ca7ea012e30bf578
rishabh246 <rishabh246@gmail.com>
2020-02-10 00:05:36 +0100

    v1 of complete replay setup

--- scripts/replay-scripts/identify-trace.sh
+++ scripts/replay-scripts/identify-trace.sh
@@ -1,26 +1,28 @@
-!/bin/bash
+#!/bin/bash
 
 # Master script to identify the precise execution path that an input takes through an NF.
 # $1 - This is the NF
 # $2 - This is the PCAP file that needs to be replayed
 
+
 BOLT_DIR=~/projects/Bolt/bolt
 KLEE_DIR=~/projects/Bolt/klee
 REPLAY_SCRIPTS_DIR=$KLEE_DIR/scripts/replay-scripts
+FN_LIST_DIR=$KLEE_DIR/scripts/stateless_scripts/fn_lists
 
 NF=$1
 PCAP=$2
 
 pushd $BOLT_DIR/nf/testbed/hard >> /dev/null
 
-  bash bench.sh $NF replay-pcap-instr "null" $PCAP
+  # bash bench.sh $NF replay-pcap-instr "null" $PCAP
   pushd $NF >> /dev/null
-    bash $REPLAY_SCRIPTS_DIR/get-last-packet-trace.sh pincounts.log replay-trace
-    python $REPLAY_SCRIPTS_DIR/demarcate-replay-trace.py replay-trace replay-demarcated
+    python $REPLAY_SCRIPTS_DIR/get-last-packet.py pincounts.log replay-trace
+    python $REPLAY_SCRIPTS_DIR/demarcate-replay-trace.py replay-trace replay-demarcated $FN_LIST_DIR/stateful_fns.txt $FN_LIST_DIR/dpdk_fns.txt $FN_LIST_DIR/time_fns.txt
     python $REPLAY_SCRIPTS_DIR/cleanup-replay-trace.py replay-demarcated replay-branches
 
     bash ../test-bolt.sh $(basename $NF)
-    python $REPLAY_SCRIPTS_DIR/match-branches.py
+    # python $REPLAY_SCRIPTS_DIR/match-branches.py
   
   popd >> /dev/null
    
