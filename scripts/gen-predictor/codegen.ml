8ba8f968aa2b9570f7f89c6caad9eb86f213bfe0
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2020-03-23 17:59:11 +0100

    Prepare parsing the constraint expressions

--- scripts/gen-predictor/codegen.ml
+++ scripts/gen-predictor/codegen.ml
@@ -1,7 +1,7 @@
 open Core
 open Ir
 
-type branch = {cond : string list; perf : int}
+type branch = {cond : tterm list; perf : int}
 
 let parse_branches file =
   let branches_raw = Str.split (Str.regexp "\n+") file in
@@ -9,14 +9,28 @@ let parse_branches file =
       let fields = Str.split (Str.regexp ", *") br in
       match fields with
       | [cond_raw; perf_raw] ->
-        let cond = Str.split (Str.regexp " *\*\*AND\*\* *") cond_raw in
+        let cond_str = Str.split (Str.regexp " *\*\*AND\*\* *") cond_raw in
         let perf = int_of_string perf_raw in
+        let cond = List.map cond_str ~f:(fun str ->
+          {v = Id str; t = Boolean})
+        in
         {cond;perf}
       | _ -> failwith (br ^ " is a malformed branch specification."))
   in
   branches
 
-let gen_predictor in_fname out_fname =
+let gen_predictor branches =
+  String.concat ~sep:"\nelse "
+    (List.map branches ~f:(fun br ->
+         "if " ^ (String.concat ~sep: " and "
+                    (List.map br.cond ~f:(fun cnd ->
+                         "(" ^ (render_tterm cnd) ^ ")"))) ^
+         " : return " ^
+         (string_of_int br.perf)
+       )) ^ "\n"
+
+
+let convert_constraints_to_predictor in_fname out_fname =
   match Sys.file_exists in_fname with
   | `No | `Unknown -> failwith ("Source constraints faile " ^ out_fname ^ "does not exist")
   | `Yes -> begin
@@ -25,16 +39,11 @@ let gen_predictor in_fname out_fname =
                      in_fname);
       let constraints = In_channel.read_all in_fname in
       let branches = parse_branches constraints in
-      let predictor = String.concat ~sep:"\n" (List.map branches ~f:(fun br ->
-          "if " ^ (String.concat ~sep: " and " br.cond) ^
-          " : return " ^
-          (string_of_int br.perf)
-        )) ^ "\n"
-      in
+      let predictor = gen_predictor branches in
       Out_channel.write_all out_fname predictor
     end
 
 let () =
   let fname = Sys.argv.(1) in
   let out_fname = fname ^ ".py" in
-  gen_predictor fname out_fname
+  convert_constraints_to_predictor fname out_fname
