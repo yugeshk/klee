277004e8e394c591d76afd8ac134a582959e7442
rishabh246 <rishabh246@gmail.com>
2020-03-30 21:50:05 +0200

    Fix stateless constructs

--- scripts/gen-predictor/domain.ml
+++ scripts/gen-predictor/domain.ml
@@ -3,13 +3,18 @@ open Ir
 
 let rewrite_rules : (term -> term option) list =
   [(function (* Stage #1 rewriting *)
-      (* user_buf[12:27] -> pkt.ether.type *)
+      (* user_buf[12:14] -> pkt.ether.type *)
       | Utility (Slice ({v=Id "user_buf";t=_}, 96, 16)) ->
         Some (Str_idx ({v=Str_idx ({v=Id "pkt";t=Unknown}, "ether");t=Unknown},
                        "type"))
-      (* buf_value[32:63] -> mbuf.packet_type *)
+      (* buf_value[32:36] -> mbuf.packet_type *)
       | Utility (Slice ({v=Id "buf_value";t=_}, 256, 32)) ->
-        Some (Str_idx ({v=Id "mbuf";t=Unknown}, "packet_type"))
+        Some (Str_idx ({v=Id "pkt";t=Unknown}, "type"))
+
+      (* user_buf[23:24] -> pkt.protocol *)
+      | Utility (Slice ({v=Id "user_buf";t=_}, 184, 8)) ->
+      Some (Str_idx ({v=Id "pkt";t=Unknown}, "protocol"))
+      
       | _ -> None);
    (function (* Stage #2 rewriting *)
       (* 0 == x -> x == 0 *)
