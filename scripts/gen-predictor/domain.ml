d03250932ad3a2fbce41c5d8962f741071578cc1
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2020-03-30 21:33:56 +0200

    Staged rewrite rules

--- scripts/gen-predictor/domain.ml
+++ scripts/gen-predictor/domain.ml
@@ -1,13 +1,17 @@
 open Core
 open Ir
 
-let rewrite (t:term) : term option =
-  match t with
-  (* user_buf[12:27] -> pkt.ether.type *)
-  | Utility (Slice ({v=Id "user_buf";t=_}, 12, 16)) ->
-    Some (Str_idx ({v=Str_idx ({v=Id "pkt";t=Unknown}, "ether");t=Unknown},
-                   "type"))
-  (* buf_value[32:63] -> mbuf.packet_type *)
-  | Utility (Slice ({v=Id "buf_value";t=_}, 32, 32)) ->
-    Some (Str_idx ({v=Id "mbuf";t=Unknown}, "packet_type"))
-  | _ -> None
+let rewrite_rules : (term -> term option) list =
+  [(function (* Stage #1 rewriting *)
+      (* user_buf[12:27] -> pkt.ether.type *)
+      | Utility (Slice ({v=Id "user_buf";t=_}, 96, 16)) ->
+        Some (Str_idx ({v=Str_idx ({v=Id "pkt";t=Unknown}, "ether");t=Unknown},
+                       "type"))
+      (* buf_value[32:63] -> mbuf.packet_type *)
+      | Utility (Slice ({v=Id "buf_value";t=_}, 256, 32)) ->
+        Some (Str_idx ({v=Id "mbuf";t=Unknown}, "packet_type"))
+      | _ -> None);
+   (function (* Stage #2 rewriting *)
+     | Bop (Eq, {v=Int i; t}, x) -> Some (Bop (Eq, x, {v=Int i; t}))
+     | _ -> None);
+  ]
