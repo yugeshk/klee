a7e5737c1621f8f52cd7cbdae02298a05ec2d449
Yugesh Kothari <yugesh.kothari@gmail.com>
2020-07-13 14:27:50 +0530

    Update falseState in Executor::fork

--- tools/klee/main.cpp
+++ tools/klee/main.cpp
@@ -450,7 +450,7 @@ public:
   void dumpCallPathTree();
   void dumpConstraintTree();
   void dumpCallPath(const ExecutionState &state, llvm::raw_ostream *file);
-  void dumpCallPathInstructions(const ExecutionState &state, llvm::raw_ostream *file);
+  void dumpCallPathInstructions(const ExecutionState &state, llvm::raw_ostream *file, unsigned id);
 };
 
 KleeHandler::KleeHandler(int argc, char **argv)
@@ -695,7 +695,7 @@ void KleeHandler::processTestCase(const ExecutionState &state,
       if(DumpCallTraceInstructions){
         std::unique_ptr<llvm::raw_fd_ostream> instr_trace_file = 
           openOutputFile(getTestFilename("ll", id));
-        dumpCallPathInstructions(state, instr_trace_file.get());
+        dumpCallPathInstructions(state, instr_trace_file.get(), id);
       }
 
       for (unsigned i = 0; i < b.numObjects; i++)
@@ -1240,8 +1240,8 @@ void KleeHandler::dumpCallPath(const ExecutionState &state,
   }
 }
 
-void KleeHandler::dumpCallPathInstructions(const ExecutionState &state, llvm::raw_ostream *file) {
-  *file << ";;-- LLVM Instruction trace --\n";
+void KleeHandler::dumpCallPathInstructions(const ExecutionState &state, llvm::raw_ostream *file, unsigned id) {
+  *file << ";;-- LLVM Instruction trace -- " << id << "\n";
   *file << "Call Stack | Current Function | Instruction\n";
   for (auto it : state.callPathInstr){
     *file << *it << "\n";
